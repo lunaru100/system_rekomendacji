13a741475fc2a2abf223bce13bdf3d42ffd45970
wb-p <wzbury@wp.pl>
2023-05-19 10:12:34 +0200

    Popular recomendations

--- Student.py
+++ Student.py
@@ -4,7 +4,5 @@ from typing import List
 @dataclass
 class Student:
     name :str
-    interests :List(str)
-    age :int
-    has_read :bool
-    how_many :int
+    mangas :List[str]
+    gatunki :List[str]
--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from collections import Counter
 from typing import List
 from Student import Student
 
-studenci :List(Student)
+import csv
+
+studenci :List[Student] = []
+with open('rekomendacje.csv') as file:
+    reader = csv.reader(file)
+    for line in reader:
+        studenci.append(Student(line[0], line[2:], line[1].split(", ")))
+
+
 popular = Counter(manga 
                   for student in studenci
-                  for manga in student.interests)
+                  for manga in student.mangas)
 
 print(popular)
