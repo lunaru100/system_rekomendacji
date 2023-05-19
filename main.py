9c14907c136fe8ab7a03fdebc44dfa0339f9306f
wb-p <wzbury@wp.pl>
2023-05-19 10:43:18 +0200

    Other function templates

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ from Student import Student
 
 import csv
 
+def most_popular(student :Student, studenci: List[Student], max_results = 5):
+    popular = Counter(manga 
+                    for student in studenci
+                    for manga in student.mangas)
+    suggestions = [manga[0] for manga in popular.most_common() if manga not in student.mangas]
+    return suggestions[:max_results]
+
+def filtrowanie_kolaboratywne_uzytkownicy():
+    pass
+
+
+def filtrowanie_kolaboratywne_zainteresowania():
+    pass
+
+def faktoryzacja_macierzy():
+    pass
+
 studenci :List[Student] = []
 with open('rekomendacje.csv') as file:
     reader = csv.reader(file)
@@ -11,8 +28,4 @@ with open('rekomendacje.csv') as file:
         studenci.append(Student(line[0], line[2:], line[1].split(", ")))
 
 
-popular = Counter(manga 
-                  for student in studenci
-                  for manga in student.mangas)
-
-print(f'Proponujemy {", ".join([key[0] for key in popular.most_common(3)])}, jako najbardziej popularne mangi')
+print(f'Dla studenta 1  proponujemy s{", ".join([key for key in most_popular(studenci[0], studenci)])}, jako najbardziej popularne mangi')
